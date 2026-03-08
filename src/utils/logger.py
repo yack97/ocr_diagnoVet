7813333ae770a3fbedfca854dd00c680d68aa5ca
yack97 <yap30600@gmail.com>
2026-03-08 02:01:45 -0300

    correccion de cors

---
+++ src/utils/logger.py
@@ -0,0 +1,22 @@
+import logging
+import sys
+
+def get_logger(name: str) -> logging.Logger:
+    """
+    Configura y retorna un logger estándar preparado para Google Cloud Logging.
+    """
+    logger = logging.getLogger(name)
+    
+    # Previene que se añadan múltiples handlers si ya está configurado
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        
+        # Formato básico para loguear por consola (Cloud Logging lo captura automáticamente)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(formatter)
+        
+        logger.addHandler(handler)
+        
+    return logger
